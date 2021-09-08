36fa7f8ec76a59b8d7117ce081cc786bc03a8e4d
mayone-du <cocomayo1201@icloud.com>
2021-09-08 12:10:50 +0900

    Fix google auth modal UI

--- src/libs/hooks/useAuthModal.tsx
+++ src/libs/hooks/useAuthModal.tsx
@@ -1,5 +1,6 @@
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, useCallback, useState } from "react";
+import { useCallback, useState } from "react";
+import { FcGoogle } from "react-icons/fc";
 import { handleSignIn } from "src/libs/functions/handleSignIn";
 
 // 認証モーダルの開閉
@@ -23,49 +24,50 @@ export const useAuthModal = () => {
   // コンポーネントを返す関数
   const renderModal = () => {
     return (
-      <Transition appear show={isOpenModal} as={Fragment}>
-        <Dialog as="div" className="overflow-y-auto fixed inset-0 z-10" onClose={handleCloseModal}>
-          <div className="px-4 min-h-screen text-center">
+      <Transition appear show={isOpenModal} as="div">
+        <Dialog
+          as="div"
+          className="overflow-y-auto fixed inset-0 z-10 bg-gray-400 bg-opacity-40"
+          onClose={handleCloseModal}
+        >
+          <div className="flex justify-center items-center px-4 min-h-screen text-center">
             <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
+              as="div"
+              enter="ease-out duration-50"
               enterFrom="opacity-0"
               enterTo="opacity-100"
-              leave="ease-in duration-200"
+              leave="ease-in duration-100"
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
               <Dialog.Overlay className="fixed inset-0" />
             </Transition.Child>
 
-            {/* This element is to trick the browser into centering the modal contents. */}
             <span className="inline-block h-screen align-middle" aria-hidden="true">
               &#8203;
             </span>
             <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
+              as="div"
+              enter="ease-out duration-100"
               enterFrom="opacity-0 scale-95"
               enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
+              leave="ease-in duration-100"
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <div className="inline-block overflow-hidden p-6 my-8 w-full max-w-md text-left align-middle bg-white rounded-2xl shadow-xl transition-all transform">
-                <Dialog.Title
-                  as="h3"
-                  className="text-lg font-medium leading-6 text-center text-gray-900"
-                >
-                  Googleでログイン
+              <div className="overflow-hidden p-6 m-auto w-96 bg-white rounded-lg shadow-xl transition-all transform">
+                <Dialog.Title as="h3" className="text-2xl font-bold text-center text-gray-900">
+                  TODO App
                 </Dialog.Title>
 
                 <div className="mt-4">
                   <button
                     type="button"
-                    className="block py-2 px-4 mx-auto text-sm font-medium text-blue-900 bg-blue-100 hover:bg-blue-200 rounded-md border border-transparent focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 focus:outline-none"
+                    className="flex items-center py-2 px-4 mx-auto bg-blue-50 hover:bg-blue-100 rounded border border-blue-200 focus-visible:ring-2 active:ring-2 ring-blue-200 focus-visible:ring-blue-500 focus-visible:ring-offset-2 shadow-sm focus:outline-none"
                     onClick={handleClick}
                   >
-                    SignIn with Google
+                    <FcGoogle className="w-6 h-6" />
+                    <span className="mx-4 font-bold">SignIn with Google</span>
                   </button>
                 </div>
               </div>
