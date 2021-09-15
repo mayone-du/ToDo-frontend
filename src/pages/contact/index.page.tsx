a5818717b093cdf512369ac3c4d22c0012504352
mayone-du <cocomayo1201@icloud.com>
2021-09-16 07:04:23 +0900

    Add contact form validation

--- src/pages/contact/index.page.tsx
+++ src/pages/contact/index.page.tsx
@@ -4,18 +4,34 @@ import { Layout } from "src/layouts";
 
 type ContactInputs = {
   title: string;
+  content: string;
 };
 
 const ContactIndexPage: CustomNextPage = () => {
-  const { register, handleSubmit } = useForm<ContactInputs>();
-  const onSubmit = (data: any) => {
-    alert(data);
+  const {
+    register,
+    // formState: { errors },
+    handleSubmit,
+  } = useForm<ContactInputs>();
+  const onSubmit = (data: ContactInputs) => {
+    alert(JSON.stringify(data));
   };
   return (
     <div>
       <h1>お問い合わせ</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" className="block border" {...register("title")} />
+        <input
+          type="text"
+          className="block border"
+          {...register("title", { required: true, maxLength: 100 })}
+        />
+        <textarea
+          {...register("content", { required: true })}
+          className="border resize-none"
+        ></textarea>
+        <button type="submit" className="block text-center rounded-md border">
+          送信
+        </button>
       </form>
     </div>
   );
