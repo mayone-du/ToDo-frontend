574ddb7dfe7d01ad2aa7f230ae025ffba94f7eb8
mayone-du <cocomayo1201@icloud.com>
2021-09-15 23:43:34 +0900

    Add sample react-hook-form codes

--- src/pages/contact/index.page.tsx
+++ src/pages/contact/index.page.tsx
@@ -1,10 +1,22 @@
 import type { CustomNextPage } from "next";
+import { useForm } from "react-hook-form";
 import { Layout } from "src/layouts";
 
+type ContactInputs = {
+  title: string;
+};
+
 const ContactIndexPage: CustomNextPage = () => {
+  const { register, handleSubmit } = useForm<ContactInputs>();
+  const onSubmit = (data: any) => {
+    alert(data);
+  };
   return (
     <div>
       <h1>お問い合わせ</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" className="block border" {...register("title")} />
+      </form>
     </div>
   );
 };
